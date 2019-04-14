416a79042fec2ba33cdd633072a68cf30c025e4d
Catherine Musemeche <catmus3@sweethome.dreamhost.com>
2019-04-13 17:03:50 -0700

    updates

--- content/contact.php
+++ content/contact.php
@@ -1,8 +1,7 @@
 <div id="contactArea">
-	<h1 class="title">Contact Dr. Kate</h1>
-	<h3><span>Email:</span> <a href="mailto:drkate@catherinemusemeche.com">drkate@catherinemusemeche.com</a></h3>
-	<h3><span>Twitter:</span> <a href="http://twitter.com/drkatem">@DrKateM</a></h3>
-	<h3>Represented by <a href="mailto:laurie@defliterary.com">Laurie Abkemeier</a>, Literary Agent for <a href="http://www.defliterary.com/">DeFiore and Company</a></h3>
-	<h3><span>Media Inquiries:</span> <a href="mailto:Barbara.L.Briggs@dartmouth.edu">Barbara.L.Briggs@dartmouth.edu</a></h3>
+    <h1 class="title">Contact Dr. Kate</h1>
+    <h2 id="contactArea"><span>Email:</span> <a href="mailto:drkate@catherinemusemeche.com">drkate@catherinemusemeche.com</a></h2>
+    <h2><span>Twitter:</span> <a href="http://twitter.com/drkatem">@DrKateM</a></h2>
+    <h2>Represented by <a href="mailto:marcy@foliolit.com">Marcy Posner</a>, Literary Agent for <a href="http://foliolit.com">Folio Literary Management</a></h2>
+    <h2>Media Inquiries: <a href="mailto:drkate@catherinemusemeche.com">drkate@catherinemusemeche.com</a></h2>
 </div>
-
